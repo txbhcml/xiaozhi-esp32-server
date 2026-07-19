bfecd811467e070bb0a0c12c62a11cb2e893f078
Dickson <txbhcml@163.com>
2026-07-19 20:15:37 +0800

    fix: 移除 SystemInitConfig 中的 @DependsOn('liquibase') 注解
    Liquibase 已禁用，不再需要依赖

--- main/manager-api/src/main/java/xiaozhi/modules/config/init/SystemInitConfig.java
+++ main/manager-api/src/main/java/xiaozhi/modules/config/init/SystemInitConfig.java
@@ -2,7 +2,6 @@ package xiaozhi.modules.config.init;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.DependsOn;
 
 import jakarta.annotation.PostConstruct;
 import xiaozhi.common.constant.Constant;
@@ -13,7 +12,6 @@ import xiaozhi.modules.device.service.DeviceAddressBookService;
 import xiaozhi.modules.sys.service.SysParamsService;
 
 @Configuration
-@DependsOn("liquibase")
 public class SystemInitConfig {
 
     @Autowired
